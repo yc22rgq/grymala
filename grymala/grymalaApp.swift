061542a16da52ab4479db86ba33e610aaff780aa
fixik <irinagomel10@gmail.com>
2025-03-26 17:54:18 +0300

    Initial Commit

---
+++ grymala/grymalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  grymalaApp.swift
+//  grymala
+//
+//  Created by Эдуард Кудянов on 26.03.25.
+//
+
+import SwiftUI
+
+@main
+struct grymalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
